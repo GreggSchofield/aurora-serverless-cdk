6aad36718f5e1ba4579b288264405366eab32a29
Gregg Ellis Schofield <schofieldgregg@gmail.com>
2022-05-01 12:15:06 +0100

    Commit the boilerplace files from cdk init

---
+++ aurora-serverless/test/aurora-serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AuroraServerless from '../lib/aurora-serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aurora-serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AuroraServerless.AuroraServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
